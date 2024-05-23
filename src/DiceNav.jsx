176aaf1cb59e4bbe2b1941bf5a00fcc77b7cdef9
Sophie Wirth <sophiewirth@MacBook-Pro.local>
2024-05-23 18:26:07 +0100

    random number generator between two int

--- src/DiceNav.jsx
+++ src/DiceNav.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 
 export default class DiceNav extends Component {
+    getRandomInt(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+    }
+
+    rollDSix(n) {
+        return this.getRandomInt(1,6)
+    }
+
     rollDice = e => {
         e.preventDefault();
-        alert('hello')
+        alert(this.rollDSix())
     }
 
     render() {
@@ -11,7 +21,7 @@ export default class DiceNav extends Component {
             <div className='nav nav-roller'>
                 <h3>Dice Select</h3>
                 <div className="dice-btn-container">
-                    <button onClick={this.rollDice}>roll</button>
+                    <button onClick={this.rollDice  }>roll</button>
                 </div>
                 <div className="dice-text-container">Dice Text Container</div>
             </div>
