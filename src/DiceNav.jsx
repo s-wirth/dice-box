d09390b8f48d61a9a45a8865187464e54bdf87c6
Sophie Wirth <sophiewirth@MacBook-Pro.local>
2024-05-23 18:21:40 +0100

    btn onclick

--- src/DiceNav.jsx
+++ src/DiceNav.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 
 export default class DiceNav extends Component {
+    rollDice = e => {
+        e.preventDefault();
+        alert('hello')
+    }
+
     render() {
         return (
             <div className='nav nav-roller'>
                 <h3>Dice Select</h3>
                 <div className="dice-btn-container">
-                    <button>roll</button>
+                    <button onClick={this.rollDice}>roll</button>
                 </div>
                 <div className="dice-text-container">Dice Text Container</div>
             </div>
